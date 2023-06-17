43f385cff7579cbb7e999bee5e651b75940b8ca9
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-06-17 14:01:18 +0600

    navbar done

---
+++ src/footer/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div class="underline">This is footer</div>;
+};
+
+export default Footer;
---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,70 @@
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar ">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Home</a>
+              </li>
+
+              <li>
+                <a>About Us</a>
+              </li>
+              <li>
+                <a>Partner with Us</a>
+              </li>
+              <li>
+                <a>Blog</a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <img className="w-28" src="/logo.png" alt="" />
+          </div>
+        </div>
+        <div className="navbar-end hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Home</a>
+            </li>
+
+            <li>
+              <a>About Us</a>
+            </li>
+            <li>
+              <a>Partner with Us</a>
+            </li>
+            <li>
+              <a>Blog</a>
+            </li>
+          </ul>
+          <a className="btn bg-[#2075f0] text-white">Download ZET</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
